b7aa56c9a34a196cec0e121dc178121a65a52083
Santhos <santhos@seznam.cz>
2008-10-28 00:06:39 +0000

    - several methods implemented - work on drawImage method started => TODO: textures

--- src/GfxSystem/GfxRenderer.cpp
+++ src/GfxSystem/GfxRenderer.cpp
@@ -40,26 +40,25 @@ uint64 GfxSystem::GfxRenderer::_GetWindowHandle()
 
 void GfxRenderer::ChangeResolution( const Point& resolution )
 {
-	//TODO
-
+	mHGE->System_SetState(HGE_SCREENWIDTH,resolution.x);
+	mHGE->System_SetState(HGE_SCREENHEIGHT,resolution.y);
 
 	gInputMgr._SetResolution(resolution.x, resolution.y);
 }
 
 Point GfxRenderer::GetResolution() const
 {
-	//TODO
-	return Point(0,0);
+	return Point(mHGE->System_GetState(HGE_SCREENWIDTH),mHGE->System_GetState(HGE_SCREENHEIGHT));
 }
 
 void GfxRenderer::SetFullscreen(bool fullscreen)
 {
-	//TODO
+	mHGE->System_SetState(HGE_WINDOWED,fullscreen);	
 }
 
 bool GfxRenderer::BeginRendering(void) 
 {
-	return /*mHGE->Gfx_BeginScene()*/false;
+	return mHGE->Gfx_BeginScene();
 }
 
 bool GfxRenderer::EndRendering(void) 
@@ -68,6 +67,7 @@ bool GfxRenderer::EndRendering(void)
 	return true;
 }
 
+// hge DWORD is unsigned long
 DWORD GetHGEColor(const Color& color)
 {
 	return ((DWORD(color.a)<<24) + (DWORD(color.r)<<16) + (DWORD(color.g)<<8) + DWORD(color.b));
@@ -80,19 +80,27 @@ bool GfxRenderer::ClearScreen(const Color& color)
 }
 
 bool GfxRenderer::DrawLine(int x1, int y1, int x2, int y2, const Pen& pen) 
-{
-	// TODO colour 
-	//g->Gfx_RenderLine(x1,y1,x2,y2
+{	
+	mHGE->Gfx_RenderLine((float) x1,(float) y1,(float) x2,(float) y2,GetHGEColor(pen.color));
 	return true;
 }
 
 bool GfxRenderer::DrawLine( const Point& begin, const Point& end, const Pen& pen )
 {
+	mHGE->Gfx_RenderLine((float) begin.x,(float) begin.y,(float) end.x,(float) end.y,GetHGEColor(pen.color));
 	return false;
 }
 
 bool GfxSystem::GfxRenderer::DrawImage( const TexturePtr& image, int32 x, int32 y, uint8 anchor /*= ANCHOR_VCENTER|ANCHOR_HCENTER*/, float32 angle /*= 0.0f*/, uint8 alpha /*= 255*/, float32 scale /*= 1.0f*/ )
 {
+	//TODO get texture size
+	/*hgeQuad q;
+	q.v[0].x = (float) x;
+	q.v[0].y = (float) y;
+	
+
+
+	mHGE->Gfx_RenderQuad(&q);*/
 	return false;
 }
 
@@ -103,6 +111,18 @@ bool GfxSystem::GfxRenderer::DrawImage( const TexturePtr& image, const Point& po
 
 bool GfxSystem::GfxRenderer::DrawImage( const TexturePtr& image, const Rect& destRect, uint8 alpha /*= 255*/ )
 {
+	//TODO textures
+	/*
+	hgeQuad q;
+	q.v[0].x = destRect.x;
+	q.v[0].y = destRect.y;
+	q.v[1].x = destRect.x + destRect.w;
+	q.v[1].y = destRect.y;
+	q.v[2].x = destRect.x + destRect.w;
+	q.v[2].y = destRect.y + destRect.h;
+	q.v[3].x = destRect.x;
+	q.v[3].y = destRect.y + destRect.h;*/
+
 	return false;
 }
 
@@ -123,6 +143,7 @@ bool GfxSystem::GfxRenderer::DrawPolygon( Point* vertices, int vertices_len, con
 
 bool GfxSystem::GfxRenderer::DrawPolygon( const std::vector<Point>& vertices, const Color& fillColor, const Pen& outline, float32 angle /*= 0.0f*/, float32 scale /*= 1.0f*/, float32 textureAngle /*= 0.0f*/, float32 textureScale /*= 1.0f*/ )
 {
+	
 	return false;
 }
 
