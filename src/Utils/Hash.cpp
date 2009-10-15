4b963608e0784640216d72a516f827c9d3060d1c
Lukas Hermann <Lukas.Hermann@seznam.cz>
2009-10-15 11:57:03 +0000

    Fixed the bug in hash function that cause application crash in case of collision. Needs testing.

--- src/Utils/Hash.cpp
+++ src/Utils/Hash.cpp
@@ -5,15 +5,10 @@
 #define HASH_INIT	0x811c9dc5
 /// Prime number used for hashing.
 #define HASH_PRIME	0x01000193
-/// If set, converts between double backslashes and one backslash.
+/// If set, converts backslash to slash.
 #define CONVERT_BACKSLASH
-/// If set, it hashes case sensitive strings into different values.
+/// If set, it hashes and store strings case insensitive.
 #define CASE_INSENSITIVE
-/// If set, it checks whether all hashes were unique.
-#define DEBUG_STRING_HASH
-
-
-#ifdef DEBUG_STRING_HASH 
 
 typedef hash_map<uint32, char*> StringHashMap;
 
@@ -23,67 +18,49 @@ static StringHashMap& GetStringHashMap(void)
 	return stringMap;
 }
 
-#endif
-
 uint32 Hash::HashString(const char* hashString, const int32 strLen)
 {
 	if (!hashString || !hashString[0])
 		return Hash::INVALID_HASH;
 
-	const unsigned char* string = (const unsigned char*)hashString;
+	// convert input to nonconst null-terminated string,
+	// hash computation and string conversion
 	int32 len  = strLen==-1 ? strlen(hashString) : strLen;
 	uint32 hash = HASH_INIT;
-
-	while (len--)
+	char* str = new char[len+1]; 
+	for (int32 i=0; i<len; ++i)
 	{
 		hash *= HASH_PRIME;
-
-		char c = *string++;
-
+		str[i] = hashString[i];
 		#ifdef CONVERT_BACKSLASH
-			if (c == '\\')
-			{
-				c = '/';
-			}
+			if (str[i] == '\\') str[i] = '/';
 		#endif
-
 		#ifdef CASE_INSENSITIVE
-			if ((c >= 'a') && (c <= 'z'))
-			{
-				c -= 'a' - 'A';
-			}
+			str[i] = (char)toupper(str[i]);
 		#endif
-
-		hash ^= (uint32)c;
+	    hash ^= (uint32)(str[i]);
 	}
-
-	#ifdef DEBUG_STRING_HASH
-		len  = strLen==-1 ? strlen(hashString) : strLen;
-		StringHashMap& stringMap = GetStringHashMap();
-		StringHashMap::const_iterator it = stringMap.find(hash);
-		if (it == stringMap.end())
+	str[len] = 0;
+	
+	// save hash and string to StringHashMap and resolve collision
+	StringHashMap& stringMap = GetStringHashMap();
+	StringHashMap::const_iterator it = stringMap.find(hash);
+	if (it == stringMap.end())
+	{
+		stringMap[hash] = str;
+	}
+	else 
+	{
+		while (strncmp(it->second, str, len+1)) // collision
 		{
-			char* str = new char[len+1];
-			for (int32 i=0; i<len; ++i)
-				str[i] = hashString[i];
-			str[len] = 0;
-			stringMap[hash] = str;
-		}
-		else if (strncmp(it->second, hashString, len))
-		{
-			if (LogSystem::LogMgr::GetSingletonPtr())
+			if ((it = stringMap.find(++hash)) == stringMap.end())
 			{
-				gLogMgr.LogMessage("HashString collision between '", it->second, "' and '", hashString, "'", LOG_ERROR);
-			}
-			else
-			{
-				const char* newString = it->second;
-				const char* oldString = hashString;
-				BS_ASSERT(!"HashString collision");
+				stringMap[hash] = str;
+				return hash;
 			}
 		}
-	#endif
-
+		delete[] str; // in stringMap is already same string as str
+	}
 	return hash;
 }
 
