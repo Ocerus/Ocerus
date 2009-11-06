6d92214fa7227b56cccccc5ae5cb35d22aa38c91
MacJariel <MacJariel@gmail.com>
2009-11-06 15:58:45 +0000

    Warning fix.

--- src/Utils/test/TestCOWPtr.cpp
+++ src/Utils/test/TestCOWPtr.cpp
@@ -83,19 +83,19 @@ SUITE(COWPtr)
 		CHECK_EQUAL(MockWithCounting<int>::RefCount(), 1U);
 		const COWPtr<MockWithCounting<int> >& rMyPtr1 = myPtr1;
 		CHECK_EQUAL(rMyPtr1->GetData(), 42);
-		CHECK_EQUAL(1, MockWithCounting<int>::RefCount());
+		CHECK_EQUAL(1U, MockWithCounting<int>::RefCount());
 		CHECK_EQUAL(const_cast<const COWPtr<MockWithCounting<int> >& >(myPtr1)->GetData(), 42);
 		CHECK_EQUAL(const_cast<const COWPtr<MockWithCounting<int> >& >(myPtr2)->GetData(), 42);
 		CHECK_EQUAL(const_cast<const COWPtr<MockWithCounting<int> >& >(myPtr3)->GetData(), 42);
-		CHECK_EQUAL(1, MockWithCounting<int>::RefCount());
+		CHECK_EQUAL(1U, MockWithCounting<int>::RefCount());
 		myPtr1->SetData(100);
-		CHECK_EQUAL(2, MockWithCounting<int>::RefCount());
+		CHECK_EQUAL(2U, MockWithCounting<int>::RefCount());
 		myPtr1->SetData(1000);
-		CHECK_EQUAL(2, MockWithCounting<int>::RefCount());
+		CHECK_EQUAL(2U, MockWithCounting<int>::RefCount());
 		myPtr2->SetData(0);
-		CHECK_EQUAL(3, MockWithCounting<int>::RefCount());
+		CHECK_EQUAL(3U, MockWithCounting<int>::RefCount());
 		myPtr3->SetData(1);
-		CHECK_EQUAL(3, MockWithCounting<int>::RefCount());
+		CHECK_EQUAL(3U, MockWithCounting<int>::RefCount());
 	}
 }
 
