bf3b13df35e874719712dec76db3c3a3dd2fd027
Hardwire <ondrej.mocny@gmail.com>
2010-01-08 15:07:33 +0000

    - debug draw pro fyziku

--- output/data/test/ConstantMover.as
+++ output/data/test/ConstantMover.as
@@ -1,5 +1,5 @@
 void OnUpdatePhysics(float32 delta)
 {
   EntityHandle handle = GetCurrentEntityHandle();
-	handle.CallFunction("ApplyForce", PropertyFunctionParameters() << Vector2(10000.0, 0.0) << Vector2(0, 0));
+	handle.CallFunction("ApplyForce", PropertyFunctionParameters() << Vector2(100000.0, 0.0) << Vector2(0, 0));
 }
--- src/Core/PhysicsDraw.cpp
+++ src/Core/PhysicsDraw.cpp
@@ -14,29 +14,37 @@ Core::PhysicsDraw::PhysicsDraw( void )
 	SetFlags(flags);
 }
 
-void Core::PhysicsDraw::DrawPolygon( const b2Vec2* vertices, int32 vertexCount, const b2Color& color )
+void Core::PhysicsDraw::DrawPolygon( const b2Vec2* vertices, int32 vertexCount, const b2Color& b2color )
 {
-
+	GfxSystem::Color color((uint8)(b2color.r*255), (uint8)(b2color.g*255), (uint8)(b2color.b*255), 255);
+	gGfxRenderer.DrawPolygon(vertices, vertexCount, color, false);
 }
 
-void Core::PhysicsDraw::DrawSolidPolygon( const b2Vec2* vertices, int32 vertexCount, const b2Color& color )
+void Core::PhysicsDraw::DrawSolidPolygon( const b2Vec2* vertices, int32 vertexCount, const b2Color& b2color )
 {
-
+	GfxSystem::Color color((uint8)(b2color.r*255), (uint8)(b2color.g*255), (uint8)(b2color.b*255), 80);
+	gGfxRenderer.DrawPolygon(vertices, vertexCount, color, true);
 }
 
-void Core::PhysicsDraw::DrawCircle( const b2Vec2& center, float32 radius, const b2Color& color )
+void Core::PhysicsDraw::DrawCircle( const b2Vec2& center, float32 radius, const b2Color& b2color )
 {
-
+	GfxSystem::Color color((uint8)(b2color.r*255), (uint8)(b2color.g*255), (uint8)(b2color.b*255), 255);
+	gGfxRenderer.DrawCircle(center, radius, color, false);
 }
 
-void Core::PhysicsDraw::DrawSolidCircle( const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color )
+void Core::PhysicsDraw::DrawSolidCircle( const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& b2color )
 {
-
+	GfxSystem::Color color((uint8)(b2color.r*255), (uint8)(b2color.g*255), (uint8)(b2color.b*255), 80);
+	gGfxRenderer.DrawCircle(center, radius, color, true);
 }
 
-void Core::PhysicsDraw::DrawSegment( const b2Vec2& p1, const b2Vec2& p2, const b2Color& color )
+void Core::PhysicsDraw::DrawSegment( const b2Vec2& p1, const b2Vec2& p2, const b2Color& b2color )
 {
-
+	GfxSystem::Color color((uint8)(b2color.r*255), (uint8)(b2color.g*255), (uint8)(b2color.b*255), 255);
+	Vector2 lineVertices[2];
+	lineVertices[0] = p1;
+	lineVertices[1] = p2;
+	gGfxRenderer.DrawLine(lineVertices, color);
 }
 
 void Core::PhysicsDraw::DrawXForm( const b2XForm& xf )
