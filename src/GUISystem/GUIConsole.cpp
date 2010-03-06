7a3f0742045ced98dbc5cf7d464488da0858d55f
MacJariel <MacJariel@gmail.com>
2010-03-06 02:27:01 +0000

    - Implemented console history. - Console only submits on enter and can be cleared with CTRL+C.

--- src/GUISystem/GUIConsole.cpp
+++ src/GUISystem/GUIConsole.cpp
@@ -10,6 +10,7 @@ using namespace GUISystem;
 
 GUIConsole::GUIConsole(): mIsInited(false), mCurrentLogLevelTreshold(0), mConsoleWidget(0), mConsolePromptWidget(0), mConsoleMessagesWidget(0)
 {
+	AppendEmptyHistoryCommand();
 }
 
 GUIConsole::~GUIConsole()
@@ -26,8 +27,7 @@ void GUIConsole::Init()
 	gCEGUIWM.getWindow("root")->addChildWindow(mConsoleWidget);
 	mConsolePromptWidget = gCEGUIWM.getWindow("ConsoleRoot/ConsolePrompt");
 	mConsoleMessagesWidget = (CEGUI::Listbox*)gCEGUIWM.getWindow("ConsoleRoot/Pane");
-	mConsolePromptWidget->subscribeEvent(CEGUI::Editbox::EventTextAccepted,
-									   CEGUI::Event::Subscriber(&GUIConsole::CommandHandler, this));
+	mConsolePromptWidget->subscribeEvent(CEGUI::Editbox::EventKeyDown, CEGUI::Event::Subscriber(&GUIConsole::OnEventKeyDown, this));
 	mIsInited = true;
 	CEGUI_EXCEPTION_END
 }
@@ -64,12 +64,104 @@ void GUIConsole::ToggleConsole()
 }
 
 
-bool GUIConsole::CommandHandler(const CEGUI::EventArgs& args)
+bool GUIConsole::OnEventKeyDown(const CEGUI::EventArgs& args)
 {
-	OC_UNUSED(args);
 	OC_DASSERT(mIsInited);
-	string message(mConsolePromptWidget->getText().c_str());
-	mConsolePromptWidget->setText("");
-	ScriptSystem::ScriptMgr::GetSingleton().ExecuteString(message.c_str());
-	return true;
+	const CEGUI::KeyEventArgs& keyArgs = static_cast<const CEGUI::KeyEventArgs&>(args);
+	bool controlPressed = keyArgs.sysKeys & CEGUI::Control;
+	switch (keyArgs.scancode)
+	{
+		case CEGUI::Key::C:
+			if (controlPressed)
+			{
+				ResetCommand();
+				return true;
+			}
+			break;
+		case CEGUI::Key::Return:
+			SubmitCommand();
+			return true;
+		case CEGUI::Key::ArrowUp:
+			HistoryUp();
+			return true;
+		case CEGUI::Key::ArrowDown:
+			HistoryDown();
+			return true;
+		default:
+			break;
+	}
+	return false;
 }
+
+void GUISystem::GUIConsole::SubmitCommand()
+{
+	string message(mConsolePromptWidget->getText().c_str());
+	if (message.empty()) return;
+	mConsolePromptWidget->setText("");
+
+	mCurrentHistoryItem->modifiedCommand.clear();
+
+	mHistory.back().originalCommand = message;
+	mHistory.back().modifiedCommand.clear();
+
+	AppendEmptyHistoryCommand();
+
+	ScriptSystem::ScriptMgr::GetSingleton().ExecuteString(message.c_str());
+}
+
+void GUISystem::GUIConsole::ResetCommand()
+{
+	mConsolePromptWidget->setText("");
+}
+
+void GUISystem::GUIConsole::HistoryUp()
+{
+	if (mCurrentHistoryItem == mHistory.begin())
+		return;
+
+	SaveCurrentHistoryCommand();
+	mCurrentHistoryItem--;
+	LoadCurrentHistoryCommand();
+}
+
+void GUISystem::GUIConsole::HistoryDown()
+{
+	if (mCurrentHistoryItem + 1 == mHistory.end())
+		return;
+
+	SaveCurrentHistoryCommand();
+	mCurrentHistoryItem++;
+	LoadCurrentHistoryCommand();
+}
+
+void GUIConsole::SaveCurrentHistoryCommand()
+{
+	string currentCommand(mConsolePromptWidget->getText().c_str());
+	if (!mCurrentHistoryItem->modifiedCommand.empty())
+	{
+		mCurrentHistoryItem->modifiedCommand = currentCommand;
+	}
+	else if (mCurrentHistoryItem->originalCommand != currentCommand)
+	{
+		mCurrentHistoryItem->modifiedCommand = currentCommand;
+	}
+}
+
+void GUIConsole::LoadCurrentHistoryCommand()
+{
+	if (!mCurrentHistoryItem->modifiedCommand.empty())
+	{
+		mConsolePromptWidget->setText(mCurrentHistoryItem->modifiedCommand);
+	}
+	else
+	{
+		mConsolePromptWidget->setText(mCurrentHistoryItem->originalCommand);
+	}
+}
+
+void GUIConsole::AppendEmptyHistoryCommand()
+{
+	mHistory.push_back(HistoryCommand(""));
+	mCurrentHistoryItem = mHistory.end() - 1;
+}
+
--- src/GUISystem/GUIConsole.h
+++ src/GUISystem/GUIConsole.h
@@ -38,11 +38,35 @@ namespace GUISystem
 		void ToggleConsole();
 
 	private:
+
+		/// This struct holds data about an item in command history.
+		struct HistoryCommand
+		{
+			/// Constructs new HistoryCommand with given string.
+			HistoryCommand(const string& command): originalCommand(command) {}
+
+			string originalCommand;	///< The original command, that was submitted.
+			string modifiedCommand;	///< The modified version of this command before submitting, or null.
+		};
+		typedef deque<HistoryCommand> HistoryCommands;
+
 		GUIConsole();
 		~GUIConsole();
 
-		/// This callback is called when user types a command to the console and presses enter.
-		bool CommandHandler(const CEGUI::EventArgs& e);
+		/// This callback is called when user presses a key.
+		bool OnEventKeyDown(const CEGUI::EventArgs&);
+
+		/// Command submitting and command history related methods.
+		//@{
+
+			void SubmitCommand();
+			void ResetCommand();
+			void HistoryUp();
+			void HistoryDown();
+			void SaveCurrentHistoryCommand();
+			void LoadCurrentHistoryCommand();
+			void AppendEmptyHistoryCommand();
+		//@}
 
 		bool mIsInited;
 		int32 mCurrentLogLevelTreshold;
@@ -51,7 +75,8 @@ namespace GUISystem
 		CEGUI::Window* mConsolePromptWidget;
 		CEGUI::Listbox* mConsoleMessagesWidget;
 
-
+		HistoryCommands mHistory;
+		HistoryCommands::iterator mCurrentHistoryItem;
 	};
 
 	inline void GUIConsole::SetLogLevelTreshold(int32 newLogLevelTreshold)
