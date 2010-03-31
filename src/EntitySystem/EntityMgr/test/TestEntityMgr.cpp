15a8fa488914014947306ca41e61e376112c1b2a
Hardwire <ondrej.mocny@gmail.com>
2010-03-31 08:16:45 +0000

    - unit testing

--- src/EntitySystem/EntityMgr/test/TestEntityMgr.cpp
+++ src/EntitySystem/EntityMgr/test/TestEntityMgr.cpp
@@ -11,17 +11,53 @@ SUITE(EntityMgr)
 		::Test::InitEntities();
 
 		EntityDescription desc;
-		EntityHandle entity;
+		EntityList entities;
 
 		desc.Reset();
 		CHECK(gEntityMgr.CreateEntity(desc) == EntityHandle::Null);
 
 		desc.Reset();
 		desc.AddComponent(CT_Transform);
-		entity = gEntityMgr.CreateEntity(desc);
-		CHECK(entity != EntityHandle::Null);
+		EntityHandle entity1 = gEntityMgr.CreateEntity(desc);
+		CHECK(entity1 != EntityHandle::Null);
 
-		//TODO pokracovat
+		desc.SetName("name");
+		EntityHandle entity2 = gEntityMgr.CreateEntity(desc);
+		CHECK(entity2 != EntityHandle::Null);
+
+		desc.SetName("name");
+		EntityHandle entity3 = gEntityMgr.CreateEntity(desc);
+		CHECK(entity3 != EntityHandle::Null);
+
+		CHECK(gEntityMgr.EntityExists(entity1));
+		CHECK(gEntityMgr.EntityExists(entity2));
+		CHECK(gEntityMgr.EntityExists(entity3));
+
+		CHECK(gEntityMgr.FindFirstEntity("name") == entity2);
+
+		gEntityMgr.GetEntitiesWithComponent(entities, CT_Sprite);
+		CHECK_EQUAL((size_t)0, entities.size());
+
+		gEntityMgr.GetEntitiesWithComponent(entities, CT_Transform);
+		CHECK_EQUAL((size_t)3, entities.size());
+
+		gEntityMgr.GetEntities(entities);
+		CHECK_EQUAL((size_t)3, entities.size());
+
+		gEntityMgr.DestroyEntity(entity2);
+		gEntityMgr.DestroyEntity(entity1);
+		CHECK(gEntityMgr.EntityExists(entity1));
+		CHECK(gEntityMgr.EntityExists(entity2));
+		gEntityMgr.ProcessDestroyQueue();
+		CHECK(!gEntityMgr.EntityExists(entity1));
+		CHECK(!gEntityMgr.EntityExists(entity2));
+
+		gEntityMgr.GetEntities(entities);
+		CHECK_EQUAL((size_t)1, entities.size());
+
+		gEntityMgr.DestroyAllEntities();
+		gEntityMgr.GetEntities(entities);
+		CHECK_EQUAL((size_t)0, entities.size());
 
 		::Test::CleanSubsystems();
 	}
